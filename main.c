0c7970a235c3699013ecc8dc1615582242dafd6f
Shichen Lu <shichenl2@gmail.com>
2017-02-06 00:06:24 -0500

    Switch FSM Implementation
    -Removed nested function calls
    -Now uses 'state' enum in main function to determine operation at each
    step
    -Fixes bug of interrupt only working once per system reset

--- main.c
+++ main.c
@@ -12,7 +12,9 @@
 #include "macros.h"
 
 void set_time(void);
-void disp_time(void);
+void date_time(void);
+void bottle_count(void);
+void bottle_time(void);
 void standby(void);
 
 const char keys[] = "123A456B789C*0#D";
@@ -23,6 +25,12 @@ const char timeset[7] = {   0x50, //Seconds
                             0x05, //Day/Date
                             0x02, //Month
                             0x17};//Year, last two digits
+enum state {
+        STANDBY,
+        DATETIME,
+        BOTTLECOUNT,
+        BOTTLETIME
+    };
 
 void main(void) {
     
@@ -53,24 +61,40 @@ void main(void) {
     //</editor-fold>
     
     ei();
-    
+        
     //set_time();
-    standby();
+    unsigned char time[7];
+    state = STANDBY;
+    
+    while(1){
+        switch(state){
+            case STANDBY:
+                standby();
+                break;
+            case DATETIME:
+                date_time();
+                break;
+            case BOTTLECOUNT:
+                bottle_count();
+                break;
+            case BOTTLETIME:
+                bottle_time();
+                break;
+        }
+    }
     
     return;
 }
 
 void interrupt isr(void){
     if (INT1IF) {
-        while(INT1IF){
-            switch(PORTB){
-                case 15:
-                    standby();
-                    break;
-                case 63:
-                    disp_time();
-                    break;
-            }
+        switch(PORTB){
+            case 15:
+                state = STANDBY;
+                break;
+            case 63:
+                state = DATETIME;
+                break;               
         }
     }
     else{
@@ -81,18 +105,15 @@ void interrupt isr(void){
         }
     }
     INT1IF = 0;
-    INT1IE = 1;
     return;
 }
 
 void standby(void){
-    while(1){
-        __lcd_home();
-        printf("standby");
-        __lcd_newline();
-        printf("PORTB: %d", (PORTB & 0xF0) >> 4);
-        __delay_ms(300);
-    }
+    __lcd_home();
+    printf("standby");
+    __lcd_newline();
+    printf("PORTB: %d", (PORTB & 0xF0) >> 4);
+    __delay_ms(300);
     return;
 }
 
@@ -106,30 +127,36 @@ void set_time(void){
     I2C_Master_Stop(); //Stop condition
 }
 
-void disp_time(void){
-    unsigned char time[7];
-    while (1) {
-        //Reset RTC memory pointer 
-        I2C_Master_Start(); //Start condition
-        I2C_Master_Write(0b11010000); //7 bit RTC address + Write
-        I2C_Master_Write(0x00); //Set memory pointer to seconds
-        I2C_Master_Stop(); //Stop condition
+void date_time(void){
+    //Reset RTC memory pointer 
+    I2C_Master_Start(); //Start condition
+    I2C_Master_Write(0b11010000); //7 bit RTC address + Write
+    I2C_Master_Write(0x00); //Set memory pointer to seconds
+    I2C_Master_Stop(); //Stop condition
 
-        //Read Current Time
-        I2C_Master_Start();
-        I2C_Master_Write(0b11010001); //7 bit RTC address + Read
-        for(unsigned char i=0;i<0x06;i++){
-            time[i] = I2C_Master_Read(1);
-        }
-        time[6] = I2C_Master_Read(0);       //Final Read without ack
-        I2C_Master_Stop();
-        
-        //LCD Display
-        __lcd_home();
-        printf("%02x/%02x/%02x        ", time[5],time[4],time[6]);    //Print date in YY/MM/DD
-        __lcd_newline();
-        printf("%02x:%02x:%02x        ", time[2],time[1],time[0]);    //HH:MM:SS
-        __delay_1s();
+    //Read Current Time
+    I2C_Master_Start();
+    I2C_Master_Write(0b11010001); //7 bit RTC address + Read
+    for(unsigned char i=0;i<0x06;i++){
+        time[i] = I2C_Master_Read(1);
     }
+    time[6] = I2C_Master_Read(0);       //Final Read without ack
+    I2C_Master_Stop();
+
+    //LCD Display
+    __lcd_home();
+    printf("%02x/%02x/%02x        ", time[5],time[4],time[6]);    //Print date in YY/MM/DD
+    __lcd_newline();
+    printf("%02x:%02x:%02x        ", time[2],time[1],time[0]);    //HH:MM:SS
+    __delay_1s();
+
+    return;
+}
+
+void bottle_count(void){
+    return;
+}
+
+void bottle_time(void){
     return;
 }
